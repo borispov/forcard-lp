7523e2bbb14dad5c231762f9ed01008117f82d3c
Boris Povolotsky <boristofu@gmail.com>
2022-08-09 13:26:08 +0300

    added poorly styled FAQ section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,13 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
-export default defineConfig({});
+import vitePreact from '@preact/preset-vite';
+
+export default defineConfig({
+    integrations: [
+        preact({ compat: true}),
+    ],
+    vite: {
+        plugins: [vitePreact()],
+    }
+});
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,38 @@
+import { useState , useRef} from 'preact/hooks';
+import "./Accordion.css";
+
+interface AccordionProps {
+	title: string;
+	children: string;
+  }
+  
+  export default function Accordion ({ title, children }: AccordionProps) {
+  
+	const [active, setActive] = useState(false);
+	const [height, setHeight] = useState('0px');
+	// TODO: Add a Chevron icon and use Rotate className
+	const rotate = 'transform duration-700 ease'
+  
+	const contentSpace = useRef<HTMLDivElement>(null);
+  
+	function toggleAccordion() {
+	  setActive((prevState) => !prevState)
+	  if (contentSpace == undefined || contentSpace.current == undefined) {
+		return;
+	  }
+	  setHeight( active ? '0px' : `${contentSpace.current?.scrollHeight * 2}px`);
+	}
+  
+	return (
+	  <div 
+		onClick={toggleAccordion}
+		class="accordion">
+		  <p class="accordion-title">{title}</p>
+		<p 
+		  ref={contentSpace}
+		//   style={{ maxHeight: `${height}`}}
+		  class={`${active && 'active'} content active`}
+		>{children}</p>
+	  </div>
+	)
+  }
